41bb05151005b5b086a8e691bd621cbe3920dccf
kainat <Kainatn002@gmail.com>
2025-05-03 14:55:57 +0200

    ..

--- myfit/src/assets/pages/Home.jsx
+++ myfit/src/assets/pages/Home.jsx
@@ -2,27 +2,35 @@ export function Home() {
   return (
     <>
       <p className="introtex">
-        Welcome to My Fit! Take control of your health by <br></br> easily tracking what
-        you eat. Our app helps you count calories, monitor<br></br> your daily intake,
-        and stay on top of your fitness goals<br></br> all in one simple, user-friendly
-        place.
+        Welcome to My Fit! Take control of your health by <br></br> easily
+        tracking what you eat. Our app helps you count calories, monitor
+        <br></br> your daily intake, and stay on top of your fitness goals
+        <br></br> all in one simple, user-friendly place.
       </p>
       <h1 className="heading">Select your Age</h1>
       <div className="age-groups">
         <div className="ageGroup1">
-          <p>Age 18-29</p>
+          <p>
+            <Link to="/1">18-29</Link>
+          </p>
         </div>
 
         <div className="ageGroup2">
-          <p>Age 30-39</p>
+          <p>
+            <Link to="/2">Age 30-39</Link>
+          </p>
         </div>
 
         <div className="ageGroup3">
-          <p>Age 40-49</p>
+          <p>
+            <Link to="/3">Age 40-49</Link>
+          </p>
         </div>
 
         <div className="ageGroup4">
-          <p>Age 50+</p>
+          <p>
+            <Link to="/4">Age 50+</Link>
+          </p>
         </div>
       </div>
     </>
