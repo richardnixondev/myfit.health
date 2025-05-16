bc78086e1126fe013d615f20e53c24b5a8a517d4
Richard Nixon <rnixonaf@gmail.com>
2025-05-16 21:33:46 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/bmi" element={<BMIGeneralView />} />
-          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
     </Router>
