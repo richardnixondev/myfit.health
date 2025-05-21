7829df7971fc02718c25fe49b715c39111dd31f1
kainat <Kainatn002@gmail.com>
2025-05-21 12:00:00 +0100

    css

--- myfit/src/App.jsx
+++ myfit/src/App.jsx
@@ -1,15 +1,18 @@
 import { BrowserRouter as Router } from "react-router-dom";
 // import React, { useState } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import { Link } from "react-router";
 import { Navbar } from "./components/Navbar";
 import "./App.css";
 import { Home } from "./assets/pages/Home";
+import { AgeGroup1 } from "./assets/pages/AgeGroup1";
 
 function App() {
   return (
     <Router>
-      <Navbar /> 
+      <Navbar />
       <Home />
+      {/* <AgeGroup1 /> */}
     </Router>
   );
 }
