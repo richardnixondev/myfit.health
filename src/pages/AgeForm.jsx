e46f234700893a3e44e28423ba8ace68af880243
Richard Nixon <rnixonaf@gmail.com>
2025-06-02 12:00:00 +0100

    new bmi form and  connecting to api

---
+++ src/pages/AgeForm.jsx
@@ -0,0 +1,101 @@
+import { useState } from "react";
+
+export function AgeForm({ ageGroup }) {
+  const [formData, setFormData] = useState({
+    weight: "",
+    height: "",
+    activityLevel: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Submitted data for age group", ageGroup, formData);
+    // Here you could save the data to state, localStorage, or send to a server
+  };
+
+  return (
+    <div className="form">
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="Name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+
+        <label>
+          Weight (kg):
+          <input
+            type="number"
+            name="weight"
+            value={formData.weight}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+
+        <label>
+          Height (cm):
+          <input
+            type="number"
+            name="height"
+            value={formData.height}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Gender:
+          <select
+            name="Gender"
+            value={formData.activityLevel}
+            onChange={handleChange}
+          >
+            <option value="">Select</option>
+            <option value="low">Female</option>
+            <option value="moderate">Male</option>
+            <option value="high">Other</option>
+          </select>
+        </label>
+        <br />
+
+        <label>
+          Activity Level:
+          <select
+            name="activityLevel"
+            value={formData.activityLevel}
+            onChange={handleChange}
+          >
+            <option value="">Select</option>
+            <option value="low">Low</option>
+            <option value="moderate">Moderate</option>
+            <option value="high">High</option>
+          </select>
+        </label>
+        <br />
+
+        <label>
+          BMI Category:
+          <select name="BMI category" onChange={handleChange}>
+            <option value="">Category</option>
+            <option value="Underweight">Underweight</option>
+            <option value="Normal weight">Normal weight</option>
+            <option value="Overweight">Overweight</option>
+            <option value="Obese">Obese</option>
+          </select>
+        </label>
+        <br />
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
