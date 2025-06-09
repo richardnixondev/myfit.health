cc35b01e726492ad55e4291cb54dc820e8ab3a51
Richard Nixon <rnixonaf@gmail.com>
2025-06-09 12:00:00 +0100

    allows host to access the front end

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  host: true, // escuta em 0.0.0.0
+  port: process.env.PORT || 4173,
+  allowedHosts: ['all'], // permite todos os hosts externos
 })
