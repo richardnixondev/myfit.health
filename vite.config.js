103ed32fbc33979390df71b80457fda69e812193
Richard Nixon <rnixonaf@gmail.com>
2025-06-12 12:00:00 +0100

    setup env pro render

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  preview: {
+  server: {
+ //dev env
     host: true,
-    port: process.env.PORT || 4173,
-    allowedHosts: ['myfit-2lu2.onrender.com']
+    port: 5173
+  },
+  preview: {
+    // setup prod env render
+    host: true,
+    port: 10000, 
+    allowedHosts: [
+      'myfit.health',
+      'myfit-2lu2.onrender.com'
+    ]
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
   }
-})
+});
+
