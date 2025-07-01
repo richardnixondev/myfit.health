89ee84adf70ee74d67bcb99c7e053509f13abc46
Richard Nixon <rnixonaf@gmail.com>
2025-07-01 12:00:00 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-//import BMIRegistrationForm from './pages/BMIRegistrationForm';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import BMIGeneralView from './pages/BMIGeneralView';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <BMIGeneralView />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/bmi" element={<BMIGeneralView />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
